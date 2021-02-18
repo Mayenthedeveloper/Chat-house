b984a7d97a0b5f3513fb90b5894867ea607db47c
Mayen Akpan <neroxma@yahoo.com>
2021-02-18 08:01:56 -0800

    updates

--- src/components/Chat/components/Navbar/Navbar.js
+++ src/components/Chat/components/Navbar/Navbar.js
@@ -7,6 +7,21 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 function Navbar(){
 
     const [showProfileOptions, setShowProfileOptions] = useState(false)
+    const [showProfileModal, setShowProfileModal] = useState(false)
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [gender, setGender] = useState('')
+    const [password, setPassword] = useState('')
+    const [avatar, setAvatar] = useState('')
+
+    const submitForm = (e) => {
+        e.preventDefault()
+
+    
+    }
+    
 
     return(
         <div id='navbar' className='card-shadow'>
@@ -19,15 +34,75 @@ function Navbar(){
                 {
                     showProfileOptions && 
                     <div id='profile-options'>
-                    <p>Update profile</p>
+                    <p onClick={() => setShowProfileModal(true)}>Update profile</p>
                     <p>Logout</p>
                 </div>
                 }
             
             {
-                <Modal>
-                    <Fragment>
-                        
+                showProfileModal &&
+                <Modal click={() => setShowProfileModal(false)}>
+                    <Fragment key='header'>
+                        <h3 className='m-0'>Update profile</h3>
+                    </Fragment>
+                    <Fragment key='body'>
+                    <form>
+                                <div className='input-field mb-1'>
+                                    <input
+                                        onChange={e => setFirstName(e.target.value)}
+                                        value={''}
+                                        required='required'
+                                        type='text'
+                                        placeholder='First name' />
+                                </div>
+
+                                <div className='input-field mb-1'>
+                                    <input
+                                        onChange={e => setLastName(e.target.value)}
+                                        value={''}
+                                        required='required'
+                                        type='text'
+                                        placeholder='Last name' />
+                                </div>
+
+                                <div className='input-field mb-1'>
+                                    <input
+                                        onChange={e => setEmail(e.target.value)}
+                                        value={''}
+                                        required='required'
+                                        type='text'
+                                        placeholder='Email' />
+                                </div>
+
+                                <div className='input-field mb-1'>
+                                    <select
+                                        onChange={e => setGender(e.target.value)}
+                                        value={''}
+                                        required='required'
+                                    >
+                                        <option value='male'>Male</option>
+                                        <option value='female'>Female</option>
+                                    </select>
+                                </div>
+
+                                <div className='input-field mb-2'>
+                                    <input
+                                        onChange={e => setPassword(e.target.value)}
+                                        value={''}
+                                        required='required'
+                                        type='password'
+                                        placeholder='Password' />
+                                </div>
+
+                                <div className='input-field mb-2'>
+                                    <input
+                                        onChange={e => setAvatar(e.target.files[0])}
+                                        type='file' />
+                                </div>
+                            </form>
+                    </Fragment>
+                    <Fragment key='footer'>
+                    <button className='btn-success' onClick={submitForm}>UPDATE</button>
                     </Fragment>
                 </Modal>
             }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,40 @@
+import { prependOnceListener } from 'process';
 import React from 'react';
 import './Modal.scss'
 
-function Modal(){
+function Modal(props){
+
+    const findByKey = (name) => props.children.map(child =>{
+        if(child.key === name)
+        return child
+    })
+
+    const closeModal = (e) => {
+        e.stopPropagation()
+
+        if(e.target.classList.contains('modal-close')){
+            return props.click()
+        }
+    }
+
+
     return(
-        <div>
+        <div className='modal-mask modal-close' onClick={closeModal}>
+            <div className='modal-wrapper'>
+                <div className='modal-container'>
+
+                    <div className="modal-header">
+                        {findByKey('header')}
+                    </div>
+                    <div className="modal-body">
+                        {findByKey('body')}   
+                    </div>
+                    <div className="modal-footer">
+                        <button  className='modal-close' onClick={closeModal}>CLOSE</button>
+                        {findByKey('footer')}   
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
